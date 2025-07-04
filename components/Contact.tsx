63c5db5a3389d2c2e370e39a3cfe93b3ebfe8f31
Rawwu <raulmtzjr7@gmail.com>
2025-07-04 14:44:48 -0500

    Update Contact component buttons to route to proper links in a new tab

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,25 +6,35 @@ export default function Contact() {
             {/* Buttons */}
             <div className="flex flex-col md:flex-row justify-center gap-4 md:gap-x-8 md:pt-10 pb-20 px-20 md:px-60">
                 {/* Email */}
-                <a 
+                <a
+                    href="mailto:raulmtzjr7@gmail.com"
                     className="text-center w-full md:w-1/4 px-8 py-2 border rounded-md text-white hover:bg-neutral-700 transition hover:cursor-pointer"
                 >
                     Email Me
                 </a>
                 {/* LinkedIn */}
-                <a 
+                <a
+                    href="https://www.linkedin.com/in/raulmtzjr/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="text-center w-full md:w-1/4 px-8 py-2 border rounded-md text-white hover:bg-neutral-700 transition hover:cursor-pointer"
                 >
                     LinkedIn
                 </a>
                 {/* GitHub */}
-                <a 
+                <a
+                    href="https://github.com/Rawwu"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="text-center w-full md:w-1/4 px-8 py-2 border rounded-md text-white hover:bg-neutral-700 transition hover:cursor-pointer"
                 >
                     GitHub
                 </a>
                 {/* Blog */}
-                <a 
+                <a
+                    href="https://elcyber.net/"
+                    target="_blank"
+                    rel="noopener noreferrer"
                     className="text-center w-full md:w-1/4 px-8 py-2 border rounded-md text-white hover:bg-neutral-700 transition hover:cursor-pointer"
                 >
                     Blog
